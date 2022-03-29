4e219b5c70385a20e97463ce37e1ef43a2bf1787
mercy-wumi <m.adams1909@gmail.com>
2022-03-29 19:28:20 +0100

    worked on useState - array and object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ErrorExample from './tutorial/1-useState/setup/1-error-example'
 import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics'
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object'
 
 function App() {
   return (
     <div className='container'>
       {/* <ErrorExample /> */}
-      <UseStateBasics />
+      {/* <UseStateBasics /> */}
+      {/* <UseStateArray /> */}
+      <UseStateObject />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data)
+  const handleRemove = (id) => {
+    let filteredPeople = people.filter(person => person.id !== id);
+    setPeople(filteredPeople)
+  }
+  return (
+    <>
+      {people.map(person => {
+        const { id, name } = person
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => handleRemove(id)}>remove</button>
+          </div>
+        )
+      })}
+      <button className='btn' onClick={() => setPeople([])}>clear items</button>
+    </>
+  );
 };
 
 export default UseStateArray;
